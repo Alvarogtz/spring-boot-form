fce6b23f167a43c237cecd2364d475432e47067a
Alvarogtz <alvaro.zabala@hotmail.es>
2023-02-19 11:22:50 +0100

    Validaciones en el formulario

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,8 +1,47 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+
+import jakarta.validation.Valid;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
+	@GetMapping("/form")
+	public String form(Model model) {
+		model.addAttribute("titulo", "Formulario usuarios");
+		model.addAttribute("usuario", new Usuario("John", "Wick", 123456));
+		return "form";
+	}
+
+	@PostMapping("/form")
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
+
+		model.addAttribute("titulo", "Resultado form");
+
+		if (result.hasErrors()) {
+			/*
+			 * Map<String,String> errores = new HashMap<>();
+			 * result.getFieldErrors().forEach(err -> { errores.put(err.getField(),
+			 * "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()
+			 * )); });
+			 */
+			// model.addAttribute("error",errores);
+			return ("form");
+		}
+
+		model.addAttribute("usuario", usuario);
+		status.setComplete();
+		return "resultado";
+	}
 }
